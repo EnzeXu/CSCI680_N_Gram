a66c9db18b92ae64b76356cc761ab96ab6bb4731
EnzeXu <xue20@wfu.edu>
2024-09-24 16:24:03 -0400

    update experiments

--- N_gram.py
+++ N_gram.py
@@ -1,44 +1,107 @@
 import random
 import os
+import pickle
+import argparse
 from collections import defaultdict
 from utils import calculate_metrics
+from tqdm import tqdm
 
 class N_gram:
     def __init__(self, N ):
         self.N = N
         self.n_grams = defaultdict(int)   # dictionary to store the N-grams tokens list and their frequencies
         self.n_minus_1_grams = defaultdict(int)    # dictionary to store the (N-1)-grams tokens list and their frequencies
-        self.probabilities = {}   # dictionary to store the probabilities of each N-gram token list
+        # self.probabilities = {}   # dictionary to store the probabilities of each N-gram token list
+        self.n_minus_1_grams_next_available = defaultdict(list)
+        self.token_dataset_train, self.token_dataset_val, self.token_dataset_test = None, None, None
+        self.generate_datasets()
+        self.generate_vocabulary_train()
 
-    def load_tokenized_files(folder_path):
+    def load_tokenized_files(self, folder_path):
         token_lists = []
         # cnt = 0
         # Iterate over all files in the tokenized folder
-        for file_name in os.listdir(folder_path):
-            if file_name.endswith(".java"):  # Ensure only .java files are processed
-                file_path = os.path.join(folder_path, file_name)
+        file_list = os.listdir(folder_path)
+        file_list = sorted([item for item in file_list if item.endswith(".java")])
+        for file_name in tqdm(file_list):
+            file_path = os.path.join(folder_path, file_name)
 
-                with open(file_path, 'r', encoding='utf-8') as f:
-                    source_code_per_file = []
-                    # Read each line (token) and store it in a list for this file
-                    per_file = [line.strip() for line in f.readlines() if line.strip()]
-                    for line in per_file:
-                        # print(each_token)
-                        only_code_token = line.split(' ')[1].strip('"')
-                        source_code_per_file.append(only_code_token)
-                    # print(source_code_per_file[-10:])
-                    token_lists.append(source_code_per_file)
-                    # cnt+=1;
+            with open(file_path, 'r', encoding='utf-8') as f:
+                lines = f.readlines()
+            source_code_per_file = []
+            # Read each line (token) and store it in a list for this file
+            per_file = [line.strip() for line in lines if line.strip()]
+            for line in per_file:
+                # print(each_token)
+                only_code_token = line.split()[1][1:-1]
+                source_code_per_file.append(only_code_token)
+            # print(source_code_per_file[-10:])
+            token_lists.append(source_code_per_file)
+            # cnt+=1;
             # if cnt == 5:
             #     break
         return token_lists
 
-    def generate_dict(self, token_lists):
+
+    def generate_datasets(self, data_dump_folder="data_dump"):
+        if not os.path.exists(data_dump_folder):
+            os.makedirs(data_dump_folder)
+        train_path = os.path.join(data_dump_folder, f"{self.N}_gram_train.pkl")
+        val_path = os.path.join(data_dump_folder, f"{self.N}_gram_val.pkl")
+        test_path = os.path.join(data_dump_folder, f"{self.N}_gram_test.pkl")
+
+        if os.path.exists(train_path):
+            print(f"Loading datasets from {data_dump_folder}: train, val and test ...")
+            with open(train_path, "rb") as f1:
+                self.token_dataset_train = pickle.load(f1)
+            with open(val_path, "rb") as f2:
+                self.token_dataset_val = pickle.load(f2)
+            with open(test_path, "rb") as f3:
+                self.token_dataset_test = pickle.load(f3)
+        else:
+            print(f"Dataset not found. Generating ...")
+            self.token_dataset_train = self.load_tokenized_files(os.path.join("data_sets", "train_set_tokenized"))
+            self.token_dataset_val = self.load_tokenized_files(os.path.join("data_sets", "val_set_tokenized"))
+            self.token_dataset_test = self.load_tokenized_files(os.path.join("data_sets", "test_set_tokenized"))
+
+            with open(train_path, "wb") as f1:
+                pickle.dump(self.token_dataset_train, f1)
+            with open(val_path, "wb") as f2:
+                pickle.dump(self.token_dataset_val, f2)
+            with open(test_path, "wb") as f3:
+                pickle.dump(self.token_dataset_test, f3)
+        print(f"Datasets are ready. train size: {len(self.token_dataset_train)}, val size: {len(self.token_dataset_val)}, test size: {len(self.token_dataset_test)}")
+
+    def generate_vocabulary_train(self, data_dump_folder="data_dump"):
+        if not os.path.exists(data_dump_folder):
+            os.makedirs(data_dump_folder)
+        vocab_path_n_grams_path = os.path.join(data_dump_folder, f"{self.N}_gram_vocab_n_grams.pkl")
+        vocab_path_n_minus_1_grams_path = os.path.join(data_dump_folder, f"{self.N}_gram_vocab_n_minus_1_grams.pkl")
+        vocab_path_n_minus_1_grams_next_available_path = os.path.join(data_dump_folder, f"{self.N}_gram_vocab_n_minus_1_grams_next_available.pkl")
+        if os.path.exists(vocab_path_n_grams_path):
+            print(f"Loading vocabularies from {data_dump_folder}: n_grams, n_minus_1_grams and n_minus_1_grams_next_available ...")
+            with open(vocab_path_n_grams_path, "rb") as f:
+                self.n_grams = pickle.load(f)
+            with open(vocab_path_n_minus_1_grams_path, "rb") as f:
+                self.n_minus_1_grams = pickle.load(f)
+            with open(vocab_path_n_minus_1_grams_next_available_path, "rb") as f:
+                self.n_minus_1_grams_next_available = pickle.load(f)
+        else:
+            print(f"Vocabulary not found. Generating ...")
+            self.update_vocabulary_from_token_lists(self.token_dataset_train)
+            with open(vocab_path_n_grams_path, "wb") as f:
+                pickle.dump(self.n_grams, f)
+            with open(vocab_path_n_minus_1_grams_path, "wb") as f:
+                pickle.dump(self.n_minus_1_grams, f)
+            with open(vocab_path_n_minus_1_grams_next_available_path, "wb") as f:
+                pickle.dump(self.n_minus_1_grams_next_available, f)
+
+    def update_vocabulary_from_token_lists(self, token_lists):
         """
         Generate N-gram dictionaries based on tokenized input
         :param token_lists: A list of tokenized sentences (lists of tokens)
         """
-        for tokens in token_lists:
+        for tokens in tqdm(token_lists):
             # Add start and end markers
             tokens = ['<s>'] * (self.N - 1) + tokens + ['</s>']
             for i in range(len(tokens) - self.N + 1):
@@ -46,11 +109,47 @@ class N_gram:
                 n_minus_1_gram = tuple(tokens[i:i + self.N - 1])
                 self.n_grams[n_gram] += 1
                 self.n_minus_1_grams[n_minus_1_gram] += 1
+                # debug
+                # if n_minus_1_gram[0] == "throws" and n_minus_1_gram[1] == "Exception" and n_minus_1_gram[2] == "{":
+                #     print(n_gram)
+                #     print(f"self.n_minus_1_grams[{n_minus_1_gram}] = {self.n_minus_1_grams[n_minus_1_gram]}")
+                #     print(f"self.n_grams[{n_gram}] = {self.n_grams[n_gram]}")
+                if n_gram[-1] not in self.n_minus_1_grams_next_available[n_minus_1_gram]:
+                    self.n_minus_1_grams_next_available[n_minus_1_gram].append(n_gram[-1])
 
-        # Compute probabilities
-        for n_gram in self.n_grams:
-            n_minus_1_gram = n_gram[:-1]
-            self.probabilities[n_gram] = self.n_grams[n_gram] / self.n_minus_1_grams[n_minus_1_gram]
+        # # Compute probabilities
+        # for n_gram in self.n_grams:
+        #     n_minus_1_gram = n_gram[:-1]
+        #     self.probabilities[n_gram] = self.n_grams[n_gram] / self.n_minus_1_grams[n_minus_1_gram]
+
+    def run_test(self, token_lists):
+        task_num = 0
+        task_success_count = 0
+        for tokens in tqdm(token_lists):
+            # Add start and end markers
+            tokens = ['<s>'] * (self.N - 1) + tokens + ['</s>']
+            task_list = []
+            for i in range(len(tokens) - self.N + 1):
+                task_list.append(tokens[i:i + self.N])
+
+            for one_task in task_list:
+                given_tokens = one_task[:-1]
+                truth_token = one_task[-1]
+                pred_token = self.predict_next_token(given_tokens)
+                task_num += 1
+                task_success_count += int(truth_token == pred_token)
+        print(f"N: {self.N}")
+        print(f"precision on the test set: {task_success_count} / {task_num} = {task_success_count / task_num * 100:.2f} %")
+
+            # for i in range(len(tokens) - self.N + 1):
+            #     n_gram = tuple(tokens[i:i + self.N])
+            #     n_minus_1_gram = tuple(tokens[i:i + self.N - 1])
+            #     self.n_grams[n_gram] += 1
+            #     self.n_minus_1_grams[n_minus_1_gram] += 1
+            #     if n_gram[-1] not in self.n_minus_1_grams_next_available[n_minus_1_gram]:
+            #         self.n_minus_1_grams_next_available[n_minus_1_gram].append(n_gram[-1])
+
+
 
     # predict
     def predict_next_token(self, prev_tokens):
@@ -60,71 +159,109 @@ class N_gram:
         :return: The predicted next token
         """
         prev_tokens = tuple(prev_tokens[-(self.N - 1):])  # Get the most recent N-1 tokens
-        possible_n_grams = {n_gram: prob for n_gram, prob in self.probabilities.items() if n_gram[:-1] == prev_tokens}
+        # print(f"prev_tokens={prev_tokens}")
+        # possible_n_grams = {n_gram: prob for n_gram, prob in self.probabilities.items() if n_gram[:-1] == prev_tokens}
+        next_available_token_list = self.n_minus_1_grams_next_available[prev_tokens]
+        next_rank_list = []
+        if len(next_available_token_list) == 0:
+            best_next_token = ";"
+        else:
+            for one_next_available_token in next_available_token_list:
+                complete_tokens = prev_tokens + (one_next_available_token,)
+                # print(f"complete_tokens={complete_tokens}")
+                n_minus_1_count = self.n_minus_1_grams[prev_tokens]
+                n_count = self.n_grams[complete_tokens]
+                if n_minus_1_count == 0:
+                    score = 0.0
+                else:
+                    score = n_count / n_minus_1_count
+                next_rank_list.append((one_next_available_token, score))
+            next_rank_list.sort(key=lambda x: -x[1])
+            # print(next_rank_list)
+            best_next_token = next_rank_list[0][0]
+        # print(f"prev_tokens: {prev_tokens}, best_next_token = \'{best_next_token}\'")
+        return best_next_token
 
-        if not possible_n_grams:
-            return "</s>"  # If no match, return end token
 
-        # Pick the next token based on probability
-        next_token = max(possible_n_grams, key=possible_n_grams.get)[-1]
-        return next_token
+        # if not possible_n_grams:
+        #     return "</s>"  # If no match, return end token
+        #
+        # # Pick the next token based on probability
+        # next_token = max(possible_n_grams, key=possible_n_grams.get)[-1]
+        # return next_token
 
-    def predict_sentence(self, input_tokens):
-        """
-        Predict a complete sentence based on input tokens
-        :param input_tokens: A list of starting tokens
-        :return: The predicted sentence (as a list of tokens)
-        """
-        sentence = input_tokens[:]
-        while sentence[-1] != "</s>":
-            next_token = self.predict_next_token(sentence)
-            sentence.append(next_token)
-            if len(sentence) > 1200:  # Prevent infinite loop in case of bad model
-                break
-        return sentence[:-1]  # Remove the end token "</s>"
+    # def predict_sentence(self, input_tokens):
+    #     """
+    #     Predict a complete sentence based on input tokens
+    #     :param input_tokens: A list of starting tokens
+    #     :return: The predicted sentence (as a list of tokens)
+    #     """
+    #     sentence = input_tokens[:]
+    #     while sentence[-1] != "</s>":
+    #         next_token = self.predict_next_token(sentence)
+    #         sentence.append(next_token)
+    #         if len(sentence) > 1200:  # Prevent infinite loop in case of bad model
+    #             break
+    #     return sentence[:-1]  # Remove the end token "</s>"
+
+
+def run():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--n', type=int, default=5, help='N of N-gram')
+    args = parser.parse_args()
+
+    n_gram_model = N_gram(args.n)
+    n_gram_model.run_test(n_gram_model.token_dataset_test)
 
 
 if __name__ == "__main__":
+    run()
+    pass
+    # N = 4
+    # n_gram_model = N_gram(N)
+    # # next_token = n_gram_model.predict_next_token(("throws", "Exception", "{"))
+    # # print(next_token)
+    # n_gram_model.run_test(n_gram_model.token_dataset_test)
 
-    # initialize model
-    N = 5
-    model = N_gram(N)
-
-    # generate train_token_lists from tokenized files like this:
-    # train_token_lists = [
-    #     ["the", "cat", "sat", "on", "the", "mat"],
-    #     ["the", "dog", "barked"],
-    #     ["the", "cat", "meowed"]
-    # ]
-    train_set_tokenized_folder = 'data_sets/train_set_tokenized'
-    train_token_lists = N_gram.load_tokenized_files(train_set_tokenized_folder)
-    # for per_token_list in train_token_lists:
-    #     print(per_token_list[-5:])
-    # print("length of train_token_lists is:", len(train_token_lists) )
-
-    # train the model
-    model.generate_dict(train_token_lists)
-    # for key, value in model.n_grams.items():
-    #     print(key,"  ", value)
-    # for key, value in model.n_minus_1_grams.items():
-    #     print(key,"  ", value)
-    # for key, value in model.probabilities.items():
-    #     print(key,"  ", value)
-
-    #test on the validation set
-    val_set_tokenized_folder = 'data_sets/val_set_tokenized'
-    val_token_lists = N_gram.load_tokenized_files(val_set_tokenized_folder)
-    # print(val_token_lists[0])
-    predict_token_lists = []
-    for test_sentence in val_token_lists:
-        predict_sentence_list = model.predict_sentence(test_sentence[:model.N-1])
-        print( "groundtruth: ", test_sentence )
-        print( "input: ", test_sentence[:model.N-1] )
-        print( "ouput: ", predict_sentence_list )
-        predict_token_lists.append(predict_sentence_list)
-        # print("Predicted sentence:", " ".join(predict_sentence_list))
-    print("length of val_token_lists is:", len(val_token_lists) )
-    # precision, recall, f1, accuracy = calculate_metrics(val_token_lists, predict_token_lists)
+    # # initialize model
+    # N = 5
+    # model = N_gram(N)
+    #
+    # # generate train_token_lists from tokenized files like this:
+    # # train_token_lists = [
+    # #     ["the", "cat", "sat", "on", "the", "mat"],
+    # #     ["the", "dog", "barked"],
+    # #     ["the", "cat", "meowed"]
+    # # ]
+    # train_set_tokenized_folder = 'data_sets/train_set_tokenized'
+    # train_token_lists = N_gram.load_tokenized_files(train_set_tokenized_folder)
+    # # for per_token_list in train_token_lists:
+    # #     print(per_token_list[-5:])
+    # # print("length of train_token_lists is:", len(train_token_lists) )
+    #
+    # # train the model
+    # model.generate_dict(train_token_lists)
+    # # for key, value in model.n_grams.items():
+    # #     print(key,"  ", value)
+    # # for key, value in model.n_minus_1_grams.items():
+    # #     print(key,"  ", value)
+    # # for key, value in model.probabilities.items():
+    # #     print(key,"  ", value)
+    #
+    # #test on the validation set
+    # val_set_tokenized_folder = 'data_sets/val_set_tokenized'
+    # val_token_lists = N_gram.load_tokenized_files(val_set_tokenized_folder)
+    # # print(val_token_lists[0])
+    # predict_token_lists = []
+    # for test_sentence in val_token_lists:
+    #     predict_sentence_list = model.predict_sentence(test_sentence[:model.N-1])
+    #     print( "groundtruth: ", test_sentence )
+    #     print( "input: ", test_sentence[:model.N-1] )
+    #     print( "ouput: ", predict_sentence_list )
+    #     predict_token_lists.append(predict_sentence_list)
+    #     # print("Predicted sentence:", " ".join(predict_sentence_list))
+    # print("length of val_token_lists is:", len(val_token_lists) )
+    # # precision, recall, f1, accuracy = calculate_metrics(val_token_lists, predict_token_lists)
 
 
     # #test on the test set
