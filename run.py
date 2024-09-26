6cb722c271478e3e54099cdef1cd87b46eaf85e8
EnzeXu <xue20@wfu.edu>
2024-09-26 00:22:06 -0400

    update package

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-from utils import *
-from N_gram import *
+from ngram.N_gram import *
+from ngram.utils import *
 
 
 def run():
     train_num_list = [100]
     test_num = 100
-    N_range = range(2, 11)
+    n_range = range(2, 11)
 
     # Prepare GitHub repos: about 6,857 Java classes and 312,971 Java functions.
     build_dataset_files(repo_names, 0, 100)
@@ -15,7 +15,7 @@ def run():
     split_the_files_in_data(input_dir="data_token", output_dir="data_processed", train_num_list=train_num_list, test_num=test_num)
     # Run the main experiments. The results will be saved to logs.csv.
     for one_train_num in train_num_list:
-        for n in N_range:
+        for n in n_range:
             n_gram_model = N_gram(n, one_train_num)
             n_gram_model.generate_datasets(
                 os.path.join("data_processed", f"train_{one_train_num}"),
